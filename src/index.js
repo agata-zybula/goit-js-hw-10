ae81c1d2e46868c08ab9e4592a7e4a150c668bd1
agata-zybula <agata.masiewicz@gmail.com>
2023-02-02 21:45:58 +0100

    update 404 else if

--- src/index.js
+++ src/index.js
@@ -44,23 +44,20 @@ let filteredArray = data => {
     ); //tworzenie li dla kazdego panstwa
   } else if (data.length === 1) {
     clearCountries();
-   countryInfo.insertAdjacentHTML(
-     //wszystkie informacje o danym panstwie
-     'beforeend',
-     `<ul class="list-style"><li class="list-item country-style">${
-       data[0].name.official
-     }</li><li class="list-item"><img src="${
-       data[0].flags.svg
-     }" alt="Flag" width="150"/></li><li class="list-item">Capital: ${
-       data[0].capital
-     }</li><li class="list-item">Population: ${
-       data[0].population
-     }</li><li class="list-item">Languages: ${Object.values(
-       data[0].languages
-     ).join(', ')}</li></ul>`
-   );
-  } else data.status === 404;
-  Notiflix.Notify.failure('Oops, there is no country with that name');
+    countryInfo.insertAdjacentHTML(
+      //wszystkie informacje o danym panstwie
+      'beforeend',
+      `<ul class="list-style"><li class="list-item country-style">${data[0].name.official
+      }</li><li class="list-item"><img src="${data[0].flags.svg
+      }" alt="Flag" width="150"/></li><li class="list-item">Capital: ${data[0].capital
+      }</li><li class="list-item">Population: ${data[0].population
+      }</li><li class="list-item">Languages: ${Object.values(
+        data[0].languages
+      ).join(', ')}</li></ul>`
+    );
+  } else if (data.status === 404) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  };
 };
 
 input.addEventListener('input', _.debounce(searchCountry, DEBOUNCE_DELAY));
